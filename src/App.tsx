fb48232cf1ea080945d5468a99872c451eb927d7
Ashton Morris <amjsdev@gmail.com>
2023-06-13 18:15:12 -0400

    update services and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import Cover from './pages/cover';
-import Offerings from './pages/offerings';
-import './App.css';
+import Cover from "./pages/cover";
+import Offerings from "./pages/offerings";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Cover />
-      <Offerings />
     </div>
   );
 }
--- src/pages/cover.tsx
+++ src/pages/cover.tsx
@@ -1,46 +1,52 @@
-import './cover.scss';
+import "./cover.scss";
 
 export default function Cover() {
-    return (
-        <div className="wrap">
-            <div className="cover">
-                <div className="spine">
-                    <div className="spine__pageNumber"> 
-                        <p>01</p>
-                    </div>
-
-                    <div className="spine__meta">
-                        <hr />
-                        <p className="spine__title">ICARUS</p>
-                        <hr />
-                    </div>
-                </div>
-                
-                <div className="subMatter">
-                    <div className="subMatter__contactInfo">
-                        <h4>Location</h4>
-                        <p>Greater New York Area</p>
-                        <p>Miami, FL</p>
-                        <p>Austin, TX</p>
-
-                        <h4>Contact</h4>
-                        <p>(973) - 223 - 0516</p>
-                        <p>amjsdev@gmail.com</p>
-                        <p><a href="https://www.linkedin.com/in/autodiplo/">LinkedIn</a></p>
-
-                        <h4>Languages</h4>
-                        <p>Spanish</p>
-                        <p>Portuguese</p>
-                        <p>French</p>
-                    </div>
-
-                    <div className="subMatter__trademark">
-                        <h4>Ashton <br /> Morris</h4>
-
-                        <p>ICARUS: A DIGITAL AGENCY © {new Date().getFullYear()}</p>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="wrap">
+      <div className="cover">
+        <div className="spine">
+          <div className="spine__meta">
+            <hr />
+            <p className="spine__title">ICARUS: A Digital Agency</p>
+            <hr />
+          </div>
         </div>
-    )
+
+        <div className="subMatter">
+          <div className="subMatter__contactInfo">
+            <h4>Contact</h4>
+            <p>+1 973-223-0516</p>
+            <p>amjsdev@gmail.com</p>
+            <p>
+              <a href="https://calendly.com/icarusdigitalagency">Calendly</a>
+            </p>
+
+            <p>
+              <a href="https://github.com/xtraVanilla/automation_fsjs/blob/main/Automation%20%26%20Full%20Stack%20JavaScript.pdf">
+                Whitepaper
+              </a>
+            </p>
+
+            <h4>Languages</h4>
+            <p>Spanish</p>
+            <p>Portuguese</p>
+            <p>French</p>
+
+            <h4>Services</h4>
+            <p>Ideation</p>
+            <p>Design</p>
+            <p>Cloud Development</p>
+            <p>Web & Mobile Development</p>
+            <p>Deployment & Hosting</p>
+          </div>
+
+          <div className="subMatter__trademark">
+            <h4>
+              Ashton <br /> Morris
+            </h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
